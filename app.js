59e7128fd18f413cf1d09e485720be5970e5c8c3
Kay <kahachang@gmail.com>
2020-09-13 20:15:24 +0800

    upload package

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 const express = require('express')
-const handlebars = require('express-handlebars') // 引入 handlebars
+const handlebars = require('express-handlebars')
+const db = require('./models')
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
+const passport = require('./config/passport')
+const methodOverride = require('method-override')
+
 const app = express()
-const port = 3000
-app.engine('handlebars', handlebars()) // Handlebars 註冊樣板引擎
-app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
+const port = process.env.PORT || 3000
+
+// 若現在環境非 production 就用 dotenv 這套
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use(methodOverride('_method'))
+app.use('/upload', express.static(__dirname + '/upload'))
+
+// 傳入變數到 res.locals 裡，讓所有 view 都能存取
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
+  next()
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+  db.sequelize.sync()
+  console.log(`The app is running on http://localhost:${port}`)
 })
+
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
+require('./routes')(app, passport)
